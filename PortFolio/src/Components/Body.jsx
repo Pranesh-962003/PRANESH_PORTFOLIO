99f51a525596aa055ec95387c6e309994a18114d
Pranesh-962003 <pranesh962003.p@gmail.com>
2024-12-25 13:33:24 +0530

    Update Body.jsx

--- PortFolio/src/Components/Body.jsx
+++ PortFolio/src/Components/Body.jsx
@@ -7,7 +7,7 @@ const Body = () => {
     <div className='flex md:flex-row flex-col p-2 w-full justify-between md:h-[77%] gap-2'>
        <div className='md:w-[60%] flex flex-col md:items-start items-center md:h-full h-[500px] md:pl-28 justify-center w-full'>
         <div className=' '>
-            <img src="/src/assets/WhatsApp Image 2024-12-16 at 13.07.17_1a2abf34.jpg" className='md:h-72 h-60 w-60 rounded-lg' alt="" />
+            <img src="./assets/WhatsApp Image 2024-12-16 at 13.07.17_1a2abf34.jpg" className='md:h-72 h-60 w-60 rounded-lg' alt="" />
         </div>
         <div className='mt-10'>
             <p className='text-5xl'>Pranesh S</p>
